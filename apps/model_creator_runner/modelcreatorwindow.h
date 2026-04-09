cbe811b6bb62cc465c16fa4422f7dc85576bc7c4
Behzad Shakouri <behzad.shakouri92@gmail.com>
2026-04-09 09:48:36 -0400

    Add unified workflow modes and remove duplicate preset exposure

--- apps/model_creator_runner/modelcreatorwindow.h
+++ apps/model_creator_runner/modelcreatorwindow.h
@@ -14,6 +14,7 @@ class QPushButton;
 class QTabWidget;
 class QTextEdit;
 class QLabel;
+class QCheckBox;
 class OHQProcessRunner;
 class SimpleLinePlotWidget;
 
@@ -36,6 +37,9 @@ private slots:
     void chooseInflowFile();
     void chooseObservationFile();
     void chooseDepthProfileFile();
+    void chooseVnBaseOhqFile();
+    void chooseVnSoilLayersFile();
+    void chooseVnMoistureLayersFile();
     void loadAdditionalCommandsFromFile();
     void applySuggestedDefaults();
     void quickGenerateRunAndSave();
@@ -84,8 +88,11 @@ private:
     void copyArtifacts(const QStringList &artifacts);
     /// Write manifest CSV for copied/discovered artifacts.
     void writeArtifactManifest(const QStringList &artifacts);
+    /// Show/hide context-sensitive and optional setup rows based on model/preset.
+    void updateFieldVisibilityForContext();
 
     QComboBox *modelTypeCombo;
+    QComboBox *workflowModeCombo;
     QLineEdit *exePathEdit;
     /// Optional executable argument template (supports {script} token).
     QLineEdit *exeArgsEdit;
@@ -104,10 +111,31 @@ private:
     QLineEdit *outputSeriesFileEdit;
     QLineEdit *observationFileEdit;
     QLineEdit *depthProfileFileEdit;
+    QLineEdit *vnBaseOhqFileEdit;
+    QLineEdit *vnSoilLayersFileEdit;
+    QLineEdit *vnMoistureLayersFileEdit;
     QLineEdit *observationObjectEdit;
     QLineEdit *observationExpressionEdit;
     QLineEdit *observationNameEdit;
     QTextEdit *additionalCommandsEdit;
+    QCheckBox *showOptionalFieldsCheck;
+    QWidget *inflowRowWidget = nullptr;
+    QWidget *modelTypeRowWidget = nullptr;
+    QWidget *presetRowWidget = nullptr;
+    QWidget *templateDirRowWidget = nullptr;
+    QWidget *generatedScriptRowWidget = nullptr;
+    QWidget *simulationStartRowWidget = nullptr;
+    QWidget *simulationEndRowWidget = nullptr;
+    QWidget *outputSeriesRowWidget = nullptr;
+    QWidget *observationFileRowWidget = nullptr;
+    QWidget *depthProfileRowWidget = nullptr;
+    QWidget *observationObjectRowWidget = nullptr;
+    QWidget *observationExpressionRowWidget = nullptr;
+    QWidget *observationNameRowWidget = nullptr;
+    QWidget *additionalCommandsRowWidget = nullptr;
+    QWidget *vnBaseRowWidget = nullptr;
+    QWidget *vnSoilRowWidget = nullptr;
+    QWidget *vnMoistureRowWidget = nullptr;
     QTabWidget *tabs;
     QTextEdit *logView;
     SimpleLinePlotWidget *inflowPlot;
