55b3995b74f97420ceb39215038efcfe98581a39
Behzad Shakouri <behzad.shakouri92@gmail.com>
2026-04-10 14:52:12 -0400

    Revert "Restore VN defaults and ksat fallbacks to legacy values"
    This reverts commit 4d9dfe99621baac0f81be52d4b8aa477fd4d6f5d.

--- apps/model_creator_runner/starter_script_builder.cpp
+++ apps/model_creator_runner/starter_script_builder.cpp
@@ -70,8 +70,8 @@ void ApplyVnKsatScaleOverrides(QString *scriptText, const StarterScriptOptions &
     if (scriptText == nullptr) {
         return;
     }
-    const QString gScale = ResolveKsatScaleString(options.ksatScaleG, options.ksatScaleAll, QStringLiteral("2.5"));
-    const QString uwScale = ResolveKsatScaleString(options.ksatScaleUw, options.ksatScaleAll, QStringLiteral("35"));
+    const QString gScale = ResolveKsatScaleString(options.ksatScaleG, options.ksatScaleAll, QStringLiteral("1.0"));
+    const QString uwScale = ResolveKsatScaleString(options.ksatScaleUw, options.ksatScaleAll, QStringLiteral("1.0"));
     scriptText->replace(QStringLiteral("K_sat_scale_factor=2.5"),
                         QStringLiteral("K_sat_scale_factor=%1").arg(gScale));
     scriptText->replace(QStringLiteral("K_sat_scale_factor=35"),
