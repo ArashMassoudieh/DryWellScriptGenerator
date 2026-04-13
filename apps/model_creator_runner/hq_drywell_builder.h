1b0e6342ca9d7962b5ec4f0ddf4ec258e6df4e46
Behzad Shakouri <behzad.shakouri92@gmail.com>
2026-04-13 16:50:28 -0400

    Add absolute inflow existence check for HQ/R/VN generation

--- apps/model_creator_runner/hq_drywell_builder.h
+++ apps/model_creator_runner/hq_drywell_builder.h
@@ -1,14 +1,20 @@
-#ifndef DRYWELL_BUILDER_H
-#define DRYWELL_BUILDER_H
+#ifndef HQ_DRYWELL_BUILDER_H
+#define HQ_DRYWELL_BUILDER_H
 
 #include "starter_script_builder.h"
+
 #include <QString>
 
-namespace DrywellBuilder
+namespace HqDrywellBuilder
 {
-    bool Build(const StarterScriptOptions &options,
-               QString *scriptText,
-               QString *errorMessage = nullptr);
-}
 
-#endif
+QString FullReferenceScript();
+
+bool AppendBaseInflowBlock(const StarterScriptOptions &options,
+                           const QString &inflow,
+                           QString *scriptText,
+                           QString *errorMessage = nullptr);
+
+} // namespace HqDrywellBuilder
+
+#endif // HQ_DRYWELL_BUILDER_H
--- apps/model_creator_runner/r_bioswale_builder.h
+++ apps/model_creator_runner/r_bioswale_builder.h
@@ -1,22 +1,20 @@
-#ifndef BIOSWALE_BUILDER_H
-#define BIOSWALE_BUILDER_H
+#ifndef R_BIOSWALE_BUILDER_H
+#define R_BIOSWALE_BUILDER_H
 
 #include "starter_script_builder.h"
+
 #include <QString>
 
-// Isolated Bioswale builder.
-// This does NOT modify Drywell or VN_Drywell logic.
-// Supported modes:
-//   - FullReference
-//   - SoftReference
-//   - LoadFromOhq
-//   - Preset
-
-namespace BioswaleBuilder
+namespace RBioswaleBuilder
 {
-    bool Build(const StarterScriptOptions &options,
-               QString *scriptText,
-               QString *errorMessage = nullptr);
-}
 
-#endif // BIOSWALE_BUILDER_H
+QString FullReferenceScript();
+
+bool AppendBaseInflowBlock(const StarterScriptOptions &options,
+                           const QString &inflow,
+                           QString *scriptText,
+                           QString *errorMessage = nullptr);
+
+} // namespace RBioswaleBuilder
+
+#endif // R_BIOSWALE_BUILDER_H
